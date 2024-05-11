ff82f2d7d5a1ab93f47ee57f0d25d8318375f615
fantastic2318 <764904579@qq.com>
2024-05-11 21:45:35 +0800

    about mysqldb

--- app.py
+++ app.py
@@ -2,11 +2,16 @@ import json
 
 from flask import Flask, url_for, request, render_template, jsonify
 from markupsafe import escape
-
 from Modules.Cat import Cat
+from Config.mysqlConfig import db
+from Modules.myDatabase import CatModel
+import pymysql
 
 app = Flask(__name__)
-
+app.config.from_object('Config.config')
+db.init_app(app)
+with app.app_context():
+    db.create_all() # 在调用create_all()之前，必须导入需要操作的模型，否则不会创建表
 # 指定触发函数的URL路径
 # 浏览器中访问路径，一定要注意、如果route中没有以反斜杠结尾，访问时就不可以加
 # 如果以反斜杠结尾 那访问时加不加都可以
@@ -74,17 +79,45 @@ def show_id(id):
 #         catlist.append(Cat(id, age, name))
 #         return render_template('cat_list.html', catlist=catlist)
 
-@app.route('/cat_add', methods=['GET', 'POST'])
+@app.route('/cat_add', methods=['POST'])
 def cat_add():
-    if request.method == 'GET':
-        return render_template('cat_add.html')
-    else:
-        id = int(request.form['id'])
-        age = request.form['age']
-        name = request.form['name']
-        catlist.append(Cat(id, age, name))
-        #return render_template('cat_list.html', catlist=catlist)
-        return {'code': 200, 'success': True, 'data': id}
+    if not request.content_type == 'application/json':
+        return {'code': 400, 'success': False, 'data': '无效的请求方式，请以json形式传递'}
+    id = int(request.json['id'])
+    age = request.json['age']
+    name = request.json['name']
+    data = CatModel.query.all()
+    for cat in data:
+        if cat.id == id:
+            return {'code': 400, 'success': False, 'data': '有重复id号'}
+    cat = CatModel(id=id, name=name, age=age)
+    db.session.add(cat)
+    db.session.commit()
+    return {'code': 200, 'success': True, 'data': id}
+
+# @app.route('/cat_add', methods=['GET', 'POST'])
+# def cat_add():
+#     if request.method == 'GET':
+#         return render_template('cat_add.html')
+#     else:
+#         if not request.content_type == 'application/json':
+#             return {'code':400, 'success':False, 'data':'无效的请求方式，请以json形式传递'}
+#         data = request.get_json()
+#         id = data['id']
+#         for tmp_cat in catlist:
+#             if tmp_cat.id == id:
+#                 return {'code': 400, 'success':False, 'data': '有重复id号'}
+#         name = data['name']
+#         age = data['age']
+#         catlist.append(Cat(id, name, age))
+#         return {'code': 200, 'success': True, 'data': id}
+
+        # id = int(request.form['id'])
+        # age = request.form['age']
+        # name = request.form['name']
+        # catlist.append(Cat(id, age, name))
+        # #return render_template('cat_list.html', catlist=catlist)
+        # return {'code': 200, 'success': True, 'data': id}
 
 # 模版 render_template()
 # 这是没有变量渲染的模版
@@ -109,10 +142,23 @@ catlist = [
 # def cat_list():
 #     return render_template('cat_list.html', catlist=catlist)
 
+# @app.get('/cat_list')
+# def cat_list():
+#     tmp_data = []
+#     for cat in catlist:
+#         tmp = {}
+#         tmp['id'] = cat.id
+#         tmp['name'] = cat.name
+#         tmp['age'] = cat.age
+#         tmp_data.append(tmp)
+#     result = {'code': 200, 'success': True, 'data': tmp_data}
+#     return jsonify(result)
+
 @app.get('/cat_list')
 def cat_list():
     tmp_data = []
-    for cat in catlist:
+    data = CatModel.query.all()
+    for cat in data:
         tmp = {}
         tmp['id'] = cat.id
         tmp['name'] = cat.name
@@ -127,14 +173,28 @@ def cat_list():
 #         if cat.id == id:
 #             return render_template('cat_detail.html', cat=cat)
 
+# @app.get('/cat_detail/<int:id>')
+# def cat_detail(id):
+#     for cat in catlist:
+#         if cat.id == id:
+#             tmp_obj = {'id': cat.id, 'name': cat.name, 'age': cat.age}
+#             #return render_template('cat_detail.html', cat=cat)
+#             return {"code": 200, 'success': True, "data": tmp_obj}
+
+
 @app.get('/cat_detail/<int:id>')
 def cat_detail(id):
-    for cat in catlist:
+    tmp_data = []
+    data = CatModel.query.all()
+    tmp = {}
+    for cat in data:
         if cat.id == id:
-            tmp_obj = {'id': cat.id, 'name': cat.name, 'age': cat.age}
-            #return render_template('cat_detail.html', cat=cat)
-            return {"code": 200, "data": tmp_obj}
-
+            tmp['id'] = cat.id
+            tmp['name'] = cat.name
+            tmp['age'] = cat.age
+            tmp_data.append(tmp)
+    result = {'code': 200, 'data': tmp_data, 'success': True}
+    return jsonify(result)
 
 
 """
@@ -167,6 +227,6 @@ def jsonify_demo():
 
 if __name__ == '__main__':
     # 终端 flask --app  app run
-    # flask --app  app run --port 5001 --debug
-    app.run()
+    # flask --app  app run -p 5001 --debug
+    app.run(host='127.0.0.1', port=5000, debug=True)
 
