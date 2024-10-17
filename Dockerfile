2fd9480ecd5b53c8cc5474b3c6e3ae32c0f0bc6b
fantastic2318 <764904579@qq.com>
2024-10-17 16:10:49 +0800

    增加配置

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,5 @@ MAINTAINER wufan2019@126.com
 WORKDIR /myApp/jenkins_home/workspace/flaskProject
 COPY . /myApp/jenkins_home/workspace/flaskProject
 RUN pip3 install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple/ || echo "Error installing requirements"
-EXPOSE 9091
+EXPOSE 5000
 CMD ["python3","app.py"]
-
---
+++ flaskweb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ "$(docker images -q flaskweb:$1 2> /dev/null)" != "" ]]; then
+	docker rmi -f flaskweb:$1
+fi
+
+docker build -t flaskweb:$1 .
+
+if [[ "$(docker inspect flaskweb 2> /dev/null | grep '"Name": "/flaskweb"')" != "" ]]; then
+	docker rm -f flaskweb
+fi
+
+docker run -d --restart=always -p 5000:5000 --name flaskweb flaskweb:$1
